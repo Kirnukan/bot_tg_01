989693fa9eef49041c7ecb92911c4b85f44b36f2
unknown <serjant.saygon@gmail.com>
2023-05-24 19:47:01 +0600

    add env files

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load("local.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("5882311478:AAEPZhtv7dog3JSzzJyve79xz-Aj4IJnGeg")
+	dotEnvApi := goDotEnvVariable("API_KEY")
+	bot, err := tgbotapi.NewBotAPI(dotEnvApi)
 	if err != nil {
 		log.Panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Kirnukan/bot_tg_01
 go 1.19
 
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/joho/godotenv v1.5.1 // indirect

9f024093ac51ec86917c75f1257deb8d4e090c68
unknown <serjant.saygon@gmail.com>
2023-05-25 17:01:40 +0600

    change structure

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -28,42 +28,59 @@ func main() {
 
 	entityService := entity.NewService()
 
+	commander := NewCommander(bot, entityService)
+
 	for update := range updates {
 		if update.Message != nil {
 
 			switch update.Message.Command() {
 			case "help":
-				helpCommand(bot, update.Message)
+				commander.Help(update.Message)
 			case "hello":
-				helloCommand(bot, update.Message)
+				commander.Hello(update.Message)
 			case "list":
-				listCommand(bot, update.Message, entityService)
+				commander.List(update.Message)
 			default:
-				defaultMessaging(bot, update.Message)
+				commander.Default(update.Message)
 			}
 		}
 	}
 }
 
-func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+type Commander struct {
+	bot           *tgbotapi.BotAPI
+	entityService *entity.Service
+}
+
+func NewCommander(
+	bot *tgbotapi.BotAPI,
+	entityService *entity.Service,
+) *Commander {
+	return &Commander{
+		bot:           bot,
+		entityService: entityService,
+	}
+}
+
+func (c *Commander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
 		"/help - help\n"+
 			"/list - list entities",
 	)
 
-	bot.Send(msg)
+	c.bot.Send(msg)
 }
 
-func helloCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+func (c *Commander) Hello(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Hello, "+inputMessage.From.UserName)
 
-	bot.Send(msg)
+	c.bot.Send(msg)
 }
 
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, entityService *entity.Service) {
+func (c *Commander) List(inputMessage *tgbotapi.Message) {
 	entitiesMsgText := "Entities List: \n\n"
 
-	entities := entityService.List()
+	entities := c.entityService.List()
 
 	for i, e := range entities {
 		entitiesMsgText += fmt.Sprintf("%d) %s\n", i+1, e.Title)
@@ -71,14 +88,14 @@ func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, entitySer
 
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, entitiesMsgText)
 
-	bot.Send(msg)
+	c.bot.Send(msg)
 }
 
-func defaultMessaging(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+func (c *Commander) Default(inputMessage *tgbotapi.Message) {
 	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
 
 	//msg.ReplyToMessageID = update.Message.MessageID
 
-	bot.Send(msg)
+	c.bot.Send(msg)
 }
